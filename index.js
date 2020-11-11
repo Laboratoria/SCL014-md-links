7a6097eac17899996d2f4808aa5d3bae3c4451f3
CatalinaJL <catalina.p.jl@gmail.com>
2020-11-11 11:28:28 -0300

    index.js: add of module fs and path from Node. Creation of variable in let about info from cli, archive path, extension file and read file (this last one is with buffer)

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const fs = require('fs');
+const path = require('path');
+
+/* Obtener información de linea de comando */
+let textConsole = process.argv[2];
+console.log(textConsole);
+
+// Ruta Archivo, False si es relativa / True si es absoluta
+
+let pathAbsolute = path.isAbsolute(textConsole);
+console.log(pathAbsolute);
+
+// Extensión de archivo
+let fileExtension= path.extname(textConsole);
+console.log(fileExtension);
+
+// Leer Archivo 
+
+let readFile = fs.readFile(textConsole, (error, data) =>{
+ if(error) throw error;
+ console.log(data);
+});
+// console.log(readFile);
+
+
+
+
+// // Leer directorios y trae archivos del directorio!
+// fs.readdir(textConsole, (files) =>{
+//   files.forEach(file =>{
+//       console.log(file);
+//       console.log(path.extname(file));
+//   });
+// });
+
+// Obtener extension de path
+// console.log(path.extname(textConsole));
+
+
