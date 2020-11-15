08682e30c63e9436130df9c6140abe998bd5f9e0
yndira <yndinatera10@gmail.com>
2020-11-15 13:20:26 -0300

    refactor

--- md-links.js
+++ md-links.js
@@ -10,83 +10,83 @@ const stats = require('./stats');
 
 module.exports = (pathname, callback) => {
 
+  let route = path.resolve(pathname);
+  
   verifyRoute(pathname, callback)
-    .then(route => {
-      verifyDir(route)
-        .then(isDirectory => {
-          if (isDirectory) {
-            console.log('es un directorio, abrelo');
-            readDir(route)
-              .then(data => {
-                data.forEach((file) => {
-                  let extFile = path.extname(file);
-                  if (extFile === '.md') {
-                    console.log(file);
-                    readFile(file, route)
-                      .then(data => {
-                        console.log(process.argv[3])
-                        if (argv.validate && argv.stats ===undefined|| argv.v && argv.s === undefined) {
-                          validate(data)
-                            .then(validatedData => {
-                              validatedData.forEach(link => {
-                                console.log(pathname, link.href, link.texto, link.status, link.statusText);
-                              })
-                            })
-                            .catch(error => console.log(error))
-                        }
-                        if (argv.stats && argv.validate === undefined || argv.s && argv.v === undefined) {
-                          stats(data)
-                        }
-                        if (argv.stats && argv.validate || argv.s && argv.v) {
-                          validate(data);
-                          stats(data)
-                        }
-                        if (process.argv[3] === undefined) {
-                          console.log(data);
-                        }
-                      })
-                      .catch(error => console.log(error))
-                  } else {
-                    console.log('no hay archivos .md')
-                  }
-                })
-              })
-              .catch(error => console.log(error));
-          }
-          else {
-            console.log('es un archivo, leelo');
-            let fileExt = path.extname(route);
-            if (fileExt === '.md') {
-              readFile(route, route)
-                .then(data => {
-                  console.log(process.argv[3])
-                  if (process.argv[3] === undefined) {
-                    console.log(data);
-                  }
-                  if (argv.stats && argv.validate === undefined || argv.s && argv.v === undefined) {
-                    stats(data)
-                  }
-                  if (argv.validate && argv.stats === undefined || argv.v && argv.s === undefined) {
-                    validate(data)
-                      .then(validatedData => {
-                        validatedData.forEach(link => {
-                          console.log(pathname, link.href, link.texto, link.status, link.statusText);
+    .then(verifyDir)
+    .then(isDirectory => {
+      if (isDirectory) {
+        console.log('es un directorio, abrelo');
+        readDir(route)
+          .then(data => {
+            data.forEach((file) => {
+              let extFile = path.extname(file);
+              if (extFile === '.md') {
+                console.log(file);
+                readFile(file, route)
+                  .then(data => {
+                    console.log(process.argv[3])
+                    if (argv.validate && argv.stats === undefined || argv.v && argv.s === undefined) {
+                      validate(data)
+                        .then(validatedData => {
+                          validatedData.forEach(link => {
+                            console.log(pathname, link.href, link.texto, link.status, link.statusText);
+                          })
                         })
-                      })
-                      .catch(error => console.log(error))
-                  }
-                  if (argv.validate && argv.stats || argv.v && argv.s) {
-                    stats(data);
-                  }
-                })
-                .catch(error => console.log(error))
-            } else {
-              console.log('no es un archivo .md')
-            }
-          }
-        })
-        .catch(error => console.log(error));
-    });
+                        .catch(error => console.log(error))
+                    }
+                    if (argv.stats && argv.validate === undefined || argv.s && argv.v === undefined) {
+                      stats(data)
+                    }
+                    if (argv.stats && argv.validate || argv.s && argv.v) {
+                      validate(data);
+                      stats(data)
+                    }
+                    if (process.argv[3] === undefined) {
+                      console.log(data);
+                    }
+                  })
+                  .catch(error => console.log(error))
+              } else {
+                console.log('no hay archivos .md')
+              }
+            })
+          })
+          .catch(error => console.log(error));
+      }
+      else {
+        console.log('es un archivo, leelo');
+        let fileExt = path.extname(route);
+        if (fileExt === '.md') {
+          readFile(route, route)
+            .then(data => {
+              console.log(process.argv[3])
+              if (process.argv[3] === undefined) {
+                console.log(data);
+              }
+              if (argv.stats && argv.validate === undefined || argv.s && argv.v === undefined) {
+                stats(data)
+              }
+              if (argv.validate && argv.stats === undefined || argv.v && argv.s === undefined) {
+                validate(data)
+                  .then(validatedData => {
+                    validatedData.forEach(link => {
+                      console.log(pathname, link.href, link.texto, link.status, link.statusText);
+                    })
+                  })
+                  .catch(error => console.log(error))
+              }
+              if (argv.validate && argv.stats || argv.v && argv.s) {
+                stats(data);
+              }
+            })
+            .catch(error => console.log(error))
+        } else {
+          console.log('no es un archivo .md')
+        }
+      }
+    })
+    .catch(error => console.log(error));
 }
 
 
--- stats.js
+++ stats.js
@@ -4,20 +4,21 @@ const validate = require('./validate');
 module.exports = (data) => {
     let uniqueLinks = [];
     let brokenLinks = [];
+    let statusOK = 200;
     console.log('Total links:', data.length)
 
     validate(data)
         .then(validatedData => {
-            //console.log(validatedData)
             validatedData.forEach(link => {
                 if (!uniqueLinks.includes(link.href)) {
                     return uniqueLinks.push(link.href)
                 }
-                 if (link.status != 200) {
-                    return brokenLinks.push(link.statusText)
+                 if (!link.status != statusOK) {
+                    return brokenLinks.push(link.status)
                 } 
             })
             console.log('Unique links:', uniqueLinks.length);
+
             if (argv.validate || argv.v){
             console.log('Broken links:', brokenLinks.length);
             } 
