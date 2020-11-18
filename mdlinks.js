0de70e2733123a5f9ab2b5e0fc538d8e6a845ce8
SolMarina <sparraa17@gmail.com>
2020-11-18 16:42:02 -0300

    pequeño cambio

--- mdlinks.js
+++ mdlinks.js
@@ -6,8 +6,6 @@ const axios = require('axios');
 const mdLinks = (filePathmdarray, validate, stats) => {
 
     const infoArray = new Array;
-
-
     //Funcion que lee el archivo
     const readFile = (filePathmd) => {
         return new Promise((resolve, reject) => {
@@ -18,7 +16,7 @@ const mdLinks = (filePathmdarray, validate, stats) => {
 
                 } else {
                     resolve(
-                        (showLinks2(data, filePathmd))
+                        (showLinks(data, filePathmd))
                     )
 
                 }
@@ -28,12 +26,9 @@ const mdLinks = (filePathmdarray, validate, stats) => {
         });
 
     };
-
-
     filePathmdarray.forEach(path => readFile(path));
 
-
-    const showLinks2 = (data, filePathmd) => {
+    const showLinks = (data, filePathmd) => {
 
         const files = md.render(data.toString());
         const dom = new JSDOM(files);
@@ -47,44 +42,39 @@ const mdLinks = (filePathmdarray, validate, stats) => {
                 file: filePathmd,
 
             }
-            infoArray.push(objectArray);
+            if (objectArray.href.startsWith("http")) {
+                infoArray.push(objectArray);
+            }
 
         });
 
         validateLinks();
-        printResultsTable();
-
-
+        // printResultsTable();
     };
     const validateLinks = () => {
         infoArray.forEach(enlace => {
             console.log(enlace.href)
+            axios.get(enlace.href)
+                .then(response => {
+                    enlace.status = response.status;
+                    console.log(enlace)
 
-            if (enlace.href.startsWith("http")) {
-                axios.get(enlace.href)
-                    .then(response => {
-                        enlace.status = response.status;
-                        console.log(enlace)
+                })
+                .catch(error => {
+                    console.log(enlace)
+                    enlace.status = error.response.status;
+                    return error.response.status;
+                })
 
-                    })
-                    .catch(error => {
-                        console.log(error.response.status);
-                        return error.response.status;
-                    })
-            } else {
-                return ("link es un about");
-
-            }
         })
-        printResultsTable();
+
     }
 
-    const printResultsTable = () => {
-        console.table(infoArray, ['href', 'file', 'status']);
-
-    };
-
-
+    /*   const printResultsTable = () => {
+          console.table(infoArray, ['href', 'file', 'status']);
+      };
+  
+   */
 }
 
 module.exports = mdLinks;
