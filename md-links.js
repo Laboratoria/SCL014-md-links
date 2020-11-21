e19a3e1cbe4c579925f87727e8e05e575b05ce62
danielablancom <blancodanielac@gmail.com>
2020-11-21 20:16:11 -0300

    file md-links added without information

--- md-links.js
+++ md-links.js
@@ -1,27 +1,3 @@
-const fetch = require('node-fetch');
-
-module.exports = (validateLinks) => {
-    const linksValidateWithFetch = validateLinks.map((element) => {
-        return fetch(element.links)
-         .then ((res) => {
-             return { 
-             href: res.url,
-             text: element.text,
-             file: element.file,
-             status: res.status,
-             statusText: res.statusText,
-         }
-         });
-     });
- 
-     Promise.all(linksValidateWithFetch)
-     .then((arrayResponse) => {
-         stats(arrayResponse);
- 
-     });
-   }
-  
-    
 
   
 
