d967900dfadf56aa567745cafdab56f5a3c71e7c
Amanda D <adnamadiaz@gmail.com>
2020-11-22 20:09:39 -0300

    Add new condition at printArrayLinks

--- modules/printers/printArrayLinks.js
+++ modules/printers/printArrayLinks.js
@@ -4,7 +4,7 @@ module.exports = (arrayLinks, argumentOption, pathIn) => {
     const newArray = arrayLinks.map(arrayFileMd => {
         return printValidateAndStatFunction(arrayFileMd, argumentOption, pathIn);
     });
-    if (argumentOption.stats === false || argumentOption.validate === false) {
+    if (argumentOption == -1 || argumentOption.stats == false || argumentOption.validate == false) {
         return arrayLinks;
     }
     return Promise.all(newArray).then(resp => {
