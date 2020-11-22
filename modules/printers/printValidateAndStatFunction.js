11f008ba8e848c4067443e45e3eeefe1734af1c7
Amanda D <adnamadiaz@gmail.com>
2020-11-22 00:03:04 -0300

    Add new return at printValidateAndStatFunction

--- modules/printers/printValidateAndStatFunction.js
+++ modules/printers/printValidateAndStatFunction.js
@@ -2,7 +2,10 @@ const validateModule = require('../validateFiles/validate-module.js');
 const statsModule = require('../statsFiles/stats-module.js');
 
 module.exports = ((arrayFileMd, argumentOption, pathIn) => {
-    if (argumentOption.stats === true && argumentOption.validate === true) {
+    if (argumentOption.stats === false && argumentOption.validate === false) {
+        return statsModule(arrayFileMd, argumentOption, pathIn);
+    } 
+    else if (argumentOption.stats === true && argumentOption.validate === true) {
         return validateModule(arrayFileMd, argumentOption, pathIn);
     }
     else if (argumentOption.validate === true) {
@@ -11,4 +14,5 @@ module.exports = ((arrayFileMd, argumentOption, pathIn) => {
     else if (argumentOption.stats === true) {
         return statsModule(arrayFileMd, argumentOption, pathIn);
     }
+    
 });
