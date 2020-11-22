1ee71f47e51cdf502d3e1adfc8071ad55fb5ef8d
Amanda D <adnamadiaz@gmail.com>
2020-11-22 00:35:39 -0300

    Add new condition por optionIn function

--- main-module.js
+++ main-module.js
@@ -8,6 +8,9 @@ const pathIn = process.argv[2];
 
 // Content option selected
 const optionIn = () => {
+    if(process.argv[3] === " "){
+        return {};
+    }
     return {
         stats: process.argv.includes('--stats'),
         validate: process.argv.includes('--validate')
